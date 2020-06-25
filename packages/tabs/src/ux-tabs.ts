55769d94d4447aa2d7892324bb298df4c617264a
Stuart Bale <stuart@stuartbale.com>
2020-06-25 16:50:22 +1200

    Layout of panel

--- packages/tabs/src/ux-tabs.ts
+++ packages/tabs/src/ux-tabs.ts
@@ -2,7 +2,8 @@ import { customElement, bindable, useView } from 'aurelia-templating';
 import { inject } from 'aurelia-dependency-injection';
 import { StyleEngine, UxComponent } from '@aurelia-ux/core';
 import { UxTabsTheme } from './ux-tabs-theme';
-import { PLATFORM } from 'aurelia-framework';
+import { PLATFORM, children } from 'aurelia-framework';
+import { UxTab } from './ux-tab';
 
 @inject(Element, StyleEngine)
 @customElement('ux-tabs')
@@ -10,6 +11,8 @@ import { PLATFORM } from 'aurelia-framework';
 export class UxTabs implements UxComponent {
   @bindable public theme: UxTabsTheme;
   @bindable public iconPosition: string = 'leading'; // or none or top
+  @children('ux-tab') public tabs: UxTab[];
+  @children('ux-tab-panel') public tabPanels: UxTab[];
 
   constructor(
     public element: HTMLElement,
@@ -25,5 +28,13 @@ export class UxTabs implements UxComponent {
     }
 
     this.styleEngine.applyTheme(newValue, this.element);
+  }
+
+  public tabsChanged() {
+    console.log('tabs changed. count is ' + this.tabs?.length);
+  }
+
+  public tabPanelsChanged() {
+    console.log('tab panels changed. count is ' + this.tabPanels?.length);
   }
 }
